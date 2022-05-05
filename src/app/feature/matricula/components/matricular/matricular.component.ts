f92f635b2328e03937265bd147751c4dd34ae910
luis.morales <luis.morales@ceiba.com.co>
2022-05-04 22:45:23 -0500

    arme le request del crear matricula

--- src/app/feature/matricula/components/matricular/matricular.component.ts
+++ src/app/feature/matricula/components/matricular/matricular.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Programa } from 'src/app/feature/programa/shared/model/programa';
 import { ProgramaService } from '../../../programa/shared/service/programa.service';
+import { MatriculaService } from '../../shared/service/matricula.service';
+import { MatriculaCrearRequest } from '../../shared/model/MatriculaCrearRequest';
 
 
 
@@ -17,14 +19,18 @@ export class MatricularComponent implements OnInit {
   public listaProgramas: Observable<Programa[]>;
   public form: FormGroup;
 
-  constructor(private  fb: FormBuilder, protected programaService: ProgramaService) {
+  constructor(
+    private  fb: FormBuilder, 
+    protected programaService: ProgramaService, 
+    protected matriculaService: MatriculaService
+    ) {
     this.form = this.fb.group({
       nombre: ['', Validators.required],
       numeroIdentificacion: ['', Validators.required],
       email: ['', Validators.required],
       ciudad: ['', Validators.required],
       direccion: ['', Validators.required],
-      programa: ['', Validators.required]
+      programa: [, Validators.required]
     })
   }
 
@@ -37,7 +43,37 @@ export class MatricularComponent implements OnInit {
   }
 
   matricularme() {
-    console.log(this.form)
+
+    let programaDB: Programa = null;
+    this.listaProgramas.subscribe(programs => {
+      const program = programs.find(program => program.id.toString() === this.form.value.programa)
+      console.log(program, this.form.value.programa);
+      programaDB = program;
+    })
+
+    const usuarioMatricula: Usuario = {
+      numeroIdentificacion: this.form.value.numeroIdentificacion,
+      nombre: this.form.value.nombre,
+      email: this.form.value.email,
+      ciudad: this.form.value.ciudad,
+      direccion: this.form.value.direccion
+    }
+
+    const matriculaCrearRequest: MatriculaCrearRequest = {
+      programa: programaDB,
+      usuarioMatricula: usuarioMatricula
+    }
+
+    console.log(matriculaCrearRequest)
   }
 
+
+}
+
+interface Usuario {
+  numeroIdentificacion: number;
+  nombre: string;
+  email: string;
+  ciudad: string;
+  direccion: string;
 }
