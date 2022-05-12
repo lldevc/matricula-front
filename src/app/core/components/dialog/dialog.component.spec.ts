971896391ec528c72124f2ab886215bfc74e49d2
luis.morales <luis.morales@ceiba.com.co>
2022-05-12 10:14:29 -0500

    Se hizo test a dialogComponent

--- src/app/core/components/dialog/dialog.component.spec.ts
+++ src/app/core/components/dialog/dialog.component.spec.ts
@@ -1,19 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DialogComponent } from './dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
+  let location: Location
+  let router: Router
+
+  const routes = [
+    {path: 'home', component: {}}
+  ] as Routes;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DialogComponent ]
+      declarations: [ DialogComponent ],
+      imports: [ 
+        RouterTestingModule.withRoutes(routes),
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+
     fixture = TestBed.createComponent(DialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +41,14 @@ describe('DialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deberia navegar a home', fakeAsync(() => {
+    router.initialNavigation();
+    let btn = fixture.debugElement.query(By.css('#btn'));
+    btn.nativeElement.click();
+    router.navigate(['/home']);
+    tick();
+    expect(router.url).toBe(`/home`)
+    expect(location.path()).toBe('/home');
+  }));
 });
